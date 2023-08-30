4d5db49a390b6e31fb100cdae211c38fb1bfa223
Mohasseb <43993890+mohasseb99@users.noreply.github.com>
2023-08-30 03:47:41 +0300

    implemented mq with blocking and non-blocking apis
    tested

--- capstone_project/lib/MessageQueue.cpp
+++ capstone_project/lib/MessageQueue.cpp
@@ -1,12 +1,13 @@
 #include "MessageQueue.h"
 #include <cstring>
 #include <cerrno>
+#include <ctime>
 
 MessageQueueSender::MessageQueueSender(const char* queueName) {
     mq = mq_open(queueName, O_WRONLY);    // here we only provide name of message queue as receiver should have created it before
     if (mq == (mqd_t)-1) {  
         int error_code = errno;
-        throw std::runtime_error("mq_open failed with error code: " + std::to_string(error_code));   // throwing exception to catch it in application use message queue to log it to logger
+        throw std::runtime_error("mq_open failed with error code: " + std::to_string(error_code));   // throwing exception to catch it in application use message queue to log it to logger daemon as this library does not have access to logger
      }
 }
 
@@ -25,7 +26,7 @@ MessageQueueReceiver::MessageQueueReceiver(char* queueName, bool isBlocking) {
     this->isBlocking = isBlocking;
     this->queueName = queueName;
     struct mq_attr attr;
-    attr.mq_flags = (isBlocking) ? 0 : O_NONBLOCK; // Set the O_NONBLOCK flag to make it non-blocking or zero to make it blocking
+    attr.mq_flags =  0;
     attr.mq_maxmsg = 10;
     attr.mq_msgsize = 1024;  // size of message
     attr.mq_curmsgs = 0;     // current messages in queue, when creating it equals zero
@@ -40,8 +41,12 @@ MessageQueueReceiver::MessageQueueReceiver(char* queueName, bool isBlocking) {
 // used if you declared message queue as non-blocking
 void MessageQueueReceiver::ReceiveMessageAsync(char* received_message, size_t max_length, unsigned int* priority) {
     if(!isBlocking){
-	ssize_t result = mq_receive(mq, received_message, max_length, priority);
-	if (result == -1 && errno != EAGAIN) { // in non blocking mode if there is no message to recieve it returns -1 and errno is EAGAIN so if errno is not equal EAGAIN that means there is a problem in receiving
+    	struct timespec timeout;
+        // Set a timeout of 1 seconds
+        timeout.tv_sec = 0;
+        timeout.tv_nsec = 0;
+	ssize_t result = mq_timedreceive(mq, received_message, max_length, priority, &timeout);
+	if (result == -1 && errno != ETIMEDOUT) { // in non blocking mode if there is no message to recieve it returns -1 and errno is ETIMEDOUT so if errno is not equal ETIMEDOUT that means there is a problem in receiving
 	    int error_code = errno;
 	    throw std::runtime_error("mq_receive failed with error code: " + std::to_string(error_code));
 	}
